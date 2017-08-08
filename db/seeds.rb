bb605d5e846d8b1c150e529c72ef6229aabea71a
Louis Fanien <louis.fanien@gmail.com>
2017-08-08 14:23:52 +0200

    todolist : done

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,9 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+Task.create(name: 'Faire exo 1', description: 'Travailler sur la data', done: false)
+Task.create(name: "Faire exo 2", description: 'Retravailler sur la data', done: false )
+Task.create(name: "Faire flashcards", description: 'Réviser', done: false )
+
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
